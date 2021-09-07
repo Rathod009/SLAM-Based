7e22cdb2032f697898f35c729d82700d0caa45d1
Dhruv <dhruv18999@gmail.com>
2021-09-07 15:13:32 +0530

    Updates Done

--- Assets/Scripts/ARViewScene.cs
+++ Assets/Scripts/ARViewScene.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lean.Touch;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,87 +12,113 @@ public class ARViewScene : MonoBehaviour
 
     public ARPlaneManager aRPlaneManager;
     public ARSessionOrigin arOrigin;
-    public GameObject indicator;
     public GameObject sofa;
-    private Pose _pose;
-    private bool _poseValid = false;
-    private bool firstPlace = false;
-    private bool firstIndicate = false;
-
+    private bool firstPlace;
     public Text infoTxt;
     public Image img;
-    public Button btnPlace;
+    public Button btnReset;
 
     void Start()
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
         sofa.SetActive(false);
-        btnPlace.gameObject.SetActive(false);
+        btnReset.gameObject.SetActive(false);
+        firstPlace = false;
+
+        if(!SceneControl.firstTime)
+        {
+            img.gameObject.SetActive(false);
+            infoTxt.gameObject.SetActive(false);
+        }
     }
 
     void Update()
     {
-        updatePose();
-        updateIndicator();
-    }
-
-    public void placeObject()
-    {
-        if(_poseValid)
+        if(SceneControl.firstTime && aRPlaneManager.trackables.count > 0)
         {
-            if (firstPlace)
+            string cntnt = "Plane Detected!\nClick on Place to Augment Sofa";
+            infoTxt.text = cntnt;
+        }
+
+
+        if(firstPlace)
+        {
+            foreach (var plane in aRPlaneManager.trackables)
             {
-                infoTxt.gameObject.SetActive(false);
+                plane.gameObject.SetActive(false);
+            }
+        }
+
+        if (!firstPlace && Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            
+            if(arOrigin.GetComponent<ARRaycastManager>().Raycast(ray,hits))
+            {
+                Pose p = hits[0].pose;
+                sofa.SetActive(true);
+                sofa.transform.position = p.position;
+                var cameraViewSide = Camera.main.transform.forward.normalized;
+                var userFaceSide = new Vector3(cameraViewSide.x * -1, 0f, cameraViewSide.z * -1).normalized;
+                sofa.transform.rotation = Quaternion.LookRotation(userFaceSide);
+            }
+            firstPlace = true;
+
+            if(SceneControl.firstTime)
+            {
+                SceneControl.firstTime = false;
                 img.gameObject.SetActive(false);
-                firstPlace = false;
+                infoTxt.gameObject.SetActive(false);
             }
-            sofa.SetActive(true);
-            sofa.transform.position = _pose.position;
-            sofa.transform.rotation = _pose.rotation;
-        }
-        
-    }
 
-    private void updateIndicator()
-    {
-        if(_poseValid)
+            btnReset.gameObject.SetActive(true);
+        }
+
+        if (firstPlace && Input.touchCount > 0)
         {
-            if(firstIndicate)
+            Debug.Log("IN");
+            if (Input.GetTouch(0).tapCount == 3)
             {
-                firstIndicate = false;
-                string cntnt = "Plane Detected!\nClick on Place to Augment Sofa";
-                infoTxt.text = cntnt;
-                firstPlace = true;
+                activeARRotate();
             }
-            indicator.SetActive(true);
-            indicator.transform.SetPositionAndRotation(_pose.position, _pose.rotation);
-            btnPlace.gameObject.SetActive(true);
-        }
-        else
-        {
-            indicator.SetActive(false);
-            btnPlace.gameObject.SetActive(false);
+            else if (Input.GetTouch(0).tapCount == 2)
+            {
+                activeARScale();
+            }
         }
     }
-    private void updatePose()
+
+
+    public void ResetAll()
     {
-        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-
-        arOrigin.GetComponent<ARRaycastManager>().Raycast(screenCenter, hits, TrackableType.PlaneEstimated);
-
-        _poseValid = hits.Count > 0 ? true : false;
-
-        if(_poseValid)
+        sofa.SetActive(false);
+        firstPlace = false;
+        foreach (var plane in aRPlaneManager.trackables)
         {
-            TrackableId planeId = hits[0].trackableId;
-            ARPlane plane = aRPlaneManager.GetPlane(planeId);
-            _pose = hits[0].pose;
-            var cameraViewSide = Camera.main.transform.forward.normalized;
-            var userFaceSide = new Vector3(cameraViewSide.x * -1, 0f, cameraViewSide.z * -1).normalized;
-            _pose.rotation = Quaternion.LookRotation(userFaceSide);
-            firstIndicate = true;
+            plane.gameObject.SetActive(true);
         }
 
     }
+
+
+    public void activeARDragMove()
+    {
+        sofa.GetComponent<DragMove>().enabled = true;
+        sofa.GetComponent<LeanPinchScale>().enabled = false;
+        sofa.GetComponent<LeanTwistRotateAxis>().enabled = false;
+    }
+    public void activeARScale()
+    {
+        sofa.GetComponent<LeanPinchScale>().enabled = true;
+        sofa.GetComponent<LeanTwistRotateAxis>().enabled = false;
+        sofa.GetComponent<DragMove>().enabled = false;
+    }
+    public void activeARRotate()
+    {
+        sofa.GetComponent<LeanPinchScale>().enabled = false;
+        sofa.GetComponent<LeanTwistRotateAxis>().enabled = true;
+        sofa.GetComponent<DragMove>().enabled = false;
+    }
+
 }
